ecf5b9588d97b3f354515b4d9f0910f6f554483f
Jonas Svirkær <jonassvirkaer@gmail.com>
2026-02-18 10:34:36 +0100

    NY

--- kingdomino.py
+++ kingdomino.py
@@ -2,13 +2,7 @@ import cv2 as cv
 import numpy as np
 import os
 
-# Main function containing the backbone of the program
-
-
 def main():
-    print("+-------------------------------+")
-    print("| King Domino points calculator |")
-    print("+-------------------------------+")
     image_path = "1.jpg"
     if not os.path.isfile(image_path):
         print("Image not found")
@@ -21,15 +15,12 @@ def main():
     for y, row in enumerate(tiles):
         for x, tile in enumerate(row):
             print(f"Tile ({x}, {y}):")
-           
 
     print("\nTerrain counts:")
     for terrain, count in terrain_counts.items():
         print(f"{terrain}: {count}")
 
 # Break a board into tiles
-
-
 def get_tiles(image):
     tiles = []
     for y in range(5):
@@ -37,39 +28,6 @@ def get_tiles(image):
         for x in range(5):
             tiles[-1].append(image[y*100:(y+1)*100, x*100:(x+1)*100])
     return tiles
-
-# Determine the type of terrain in a tile
-
-
-    hsv_tile = cv.cvtColor(tile, cv.COLOR_BGR2HSV)
-    hue, saturation, value = np.median(hsv_tile, axis=(0, 1))
-    print(f"H: {hue}, S: {saturation}, V: {value}")
-
-    if 34 <= hue <= 48 and 169 <= saturation <= 248 and 73 <= value <= 164:
-        return "Grassland"
-
-    if 28 <= hue <= 77 and 68 <= saturation <= 239 and 25 <= value <= 128:
-        return "Forest"
-
-    if 78 <= hue <= 109 and 193 <= saturation <= 255 and 109 <= value <= 204:
-        return "Lake"
-
-    if 17 <= hue <= 26 and 23 <= saturation <= 181 and 68 <= value <= 144:
-        return "Swamp"
-
-    if 17 <= hue <= 30 and 34 <= saturation <= 159 and 24 <= value <= 95:
-        return "Mine"
-
-    if 20 <= hue <= 50 and 192 <= saturation <= 255 and 105 <= value <= 206:
-        return "Field"
-
-    if 18 <= hue <= 46 and 110 <= saturation <= 222 and 70 <= value <= 184:
-        return "Empty Space"
-
-    if 18 <= hue <= 39 and 54 <= saturation <= 181 and 64 <= value <= 150:
-        return "Home"
-    return "Unknown"
-
 
 if __name__ == "__main__":
     main()
