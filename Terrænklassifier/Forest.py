fe835c192b4dd935ce11c0d16dc9443e3d750627
Jonas Svirkær <jonassvirkaer@gmail.com>
2026-02-18 11:36:27 +0100

    PUSHER

--- Terrænklassifier/Forest.py
+++ Terrænklassifier/Forest.py
@@ -11,11 +11,11 @@ brikker = cv2.imread("1.jpg")
 brikker_hsv = cv2.cvtColor(brikker, cv2.COLOR_BGR2HSV)
 
 # Forest-brik range
-lower_green = np.array([28, 68, 25])
-upper_green = np.array([78, 239, 128])
+lower_forest = np.array([50, 68, 25])
+upper_forest = np.array([78, 239, 128])
 
 # Lav maske (hvid = pixels indenfor range)
-mask_forest = cv2.inRange(brikker_hsv, lower_green, upper_green)
+mask_forest = cv2.inRange(brikker_hsv, lower_forest, upper_forest)
 
 # Behold KUN pixels i masken
 result_forest = cv2.bitwise_and(brikker, brikker, mask=mask_forest)
